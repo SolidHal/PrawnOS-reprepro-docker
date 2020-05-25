f8cf4dea6d6965caa1cd6ccd323bc7c8f7a9bb1c
SolidHal <hal@halemmerich.com>
2020-05-25 12:04:51 -0700

    show commands when cleaning

--- clean-all.sh
+++ clean-all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export CONFIG_FOLDER=~/reprepro-dockerfs/reprepro-config/
-export REPO_FOLDER=~/reprepro-dockerfs/repos/
+set -x
+
+export CONFIG_FOLDER=~/reprepro-dockerfs/reprepro-config
+export REPO_FOLDER=~/reprepro-dockerfs/repos
 
 rm -rf $REPO_FOLDER/*
 rm -rf $CONFIG_FOLDER/var
