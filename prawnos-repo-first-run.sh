8462b3a15f83e3655fa11ccca5bd255865f95bf7
SolidHal <hal@halemmerich.com>
2020-05-25 11:07:54 -0700

    keep the repo between rebuilds

--- prawnos-repo-first-run.sh
+++ prawnos-repo-first-run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-export CONFIG_FOLDER=~/reprepro-config/
+
+mkdir -p ~/reprepro-dockerfs
+mkdir -p ~/reprepro-dockerfs/reprepro-config
+mkdir -p ~/reprepro-dockerfs/repos
+
+export CONFIG_FOLDER=~/reprepro-dockerfs/reprepro-config/
+export REPO_FOLDER=~/reprepro-dockerfs/repos/
 export HOSTNAME="deb.prawnos.com"
 export PROJECT_NAME="PrawnOS"
 export CODE_NAME="buster"
 export WEBSERVER_PORT=8080
 export SSH_PORT=2222
 
-docker run -v $CONFIG_FOLDER:/srv/ -p $WEBSERVER_PORT:80 -p $SSH_PORT:22 \
+docker run -v $CONFIG_FOLDER:/srv/ -v $REPO_FOLDER:/var/www/repos/
+       -p $WEBSERVER_PORT:80 -p $SSH_PORT:22 \
        -e HOSTNAME=$HOSTNAME \
        -e PROJECT_NAME=$PROJECT_NAME \
        -e CODE_NAME=$CODE_NAME \
--- prawnos-repo-run.sh
+++ prawnos-repo-run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-CONFIG_FOLDER=~/reprepro-config/
+
+
+export CONFIG_FOLDER=~/reprepro-dockerfs/reprepro-config/
+export REPO_FOLDER=~/reprepro-dockerfs/repos/
 WEBSERVER_PORT=8080
 SSH_PORT=2222
 
 IMAGE_NAME="prawnos/reprepro"
 
 echo -e "Configuration from: '$CONFIG_FOLDER'\n" \
+        "Repos folder from: '$REPO_FOLDER'\n" \
         "Webserver mapped to: '$WEBSERVER_PORT'\n" \
         "SSH daemon mapped to: '$SSH_PORT'\n" \
         ""
 
-echo "Image built succesfully; starting the build command!"
-docker run -v $CONFIG_FOLDER:/srv/ -p $WEBSERVER_PORT:80 -p $SSH_PORT:22 -d --restart always $IMAGE_NAME
+docker run -v $CONFIG_FOLDER:/srv/ -v $REPO_FOLDER:/var/www/repos/ -p $WEBSERVER_PORT:80 -p $SSH_PORT:22 -d --restart always $IMAGE_NAME
 
