a4fbd4ce25da57b4dcf8b3f5b78f69c70607d286
SolidHal <hal@halemmerich.com>
2020-05-25 11:09:20 -0700

    fix typo

--- prawnos-repo-first-run.sh
+++ prawnos-repo-first-run.sh
@@ -13,7 +13,7 @@ export CODE_NAME="buster"
 export WEBSERVER_PORT=8080
 export SSH_PORT=2222
 
-docker run -v $CONFIG_FOLDER:/srv/ -v $REPO_FOLDER:/var/www/repos/
+docker run -v $CONFIG_FOLDER:/srv/ -v $REPO_FOLDER:/var/www/repos/ \
        -p $WEBSERVER_PORT:80 -p $SSH_PORT:22 \
        -e HOSTNAME=$HOSTNAME \
        -e PROJECT_NAME=$PROJECT_NAME \
